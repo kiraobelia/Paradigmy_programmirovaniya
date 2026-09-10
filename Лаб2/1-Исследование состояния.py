43a56e34a0a6235717b5839d0a1c43dc69bd4b70
kiraobelia <158697825+kiraobelia@users.noreply.github.com>
2026-09-10 13:55:22 +0500

    Update and rename 1-Исследование состояния to 1-Исследование состояния.py

---
+++ Лаб2/1-Исследование состояния.py
@@ -0,0 +1,9 @@
+x = 10
+print(f"Шаг 1: x = {x}")
+x = x + 5
+print(f"Шаг 2: x = {x}")
+x = x - 8
+print(f"Шаг 3: x = {x}")
+x = x // 2
+print(f"Шаг 4: x = {x}")
+print(f"\nИтоговое значиение x: {x}")
