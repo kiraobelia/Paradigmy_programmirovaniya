91e6e5f0cbee844d5c2bb15453fbdcbe7359fddb
kiraobelia <158697825+kiraobelia@users.noreply.github.com>
2026-09-10 14:11:31 +0500

    Update Задание 3.py
    Управление выполнением с помощью условия

--- Лаб2/Задание 3.py
+++ Лаб2/Задание 3.py
@@ -1,1 +1,19 @@
+score_input = input("Введите балл (от 0 до 100): ")
 
+# Проверка ввода на число
+try:
+    score = float(score_input)
+    
+    # Проверка диапазона и определение оценки
+    if score < 0 or score > 100:
+        print("Ошибка: балл должен быть в диапазоне от 0 до 100.")
+    elif score >= 90:
+        print("Результат: A")
+    elif score >= 75:
+        print("Результат: B")
+    elif score >= 50:
+        print("Результат: C")
+    else:
+        print("Результат: F")
+except ValueError:
+    print("Ошибка: введено не числовое значение.")
